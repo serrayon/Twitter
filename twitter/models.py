c52c5758eeb27951e1644703dd0cf35ae56ec1ac
octavio <serrayon81@gmail.com>
2019-08-08 10:47:55 -0700

    adds models

--- twitter/models.py
+++ twitter/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+  avatar = photo_url = models.TextField()
 
+  def __str__(self):
+    return self.avatar
+    
 class Post(models.Model):
-  author = models.CharField(max_length=100)
-  title = models.CharField(max_length=100)
-  body = models.CharField(max_length=100)
+  content = models.CharField(max_length=160)
+  photo_url = models.TextField()
+  date = models.DateTimeField(auto_now=False, auto_now_add=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+  def __str__(self):
+    return f"{self.content}-{self.photo_url}"
 
 class Comment(models.Model):
-    author = models.CharField(max_length=100)
-    body = models.CharField(max_length=100)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  message = models.CharField(max_length=160)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+  date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+  def __str__(self):
+    return f"{self.title}-{self.artist}"
+
+
