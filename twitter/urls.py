469a1fa9a19c2a45b317f9ecb7c76254792ec64f
hungfaikoo <jhk88@hotmail.com>
2019-08-08 13:38:59 -0700

    info updated

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#   path('', views.home, name='home')
-# ]
+urlpatterns = [
+  path('', views.home, name='home'),
+  path('status', views.json_res, name='status')
+]
