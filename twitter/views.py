326f1ec8d048afed4818fb8859ba6f9f43a7ee65
octavio <serrayon81@gmail.com>
2019-08-08 16:40:04 -0700

    adds post-detail,list, and forms

--- twitter/views.py
+++ twitter/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
 from .models import Post, Comment
+from .forms import PostForm, CommentForm, ProfileForm
 
 def home(request):
     return HttpResponse("You are home")
@@ -21,3 +22,48 @@ def comment_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(id=pk)
     return render(request, 'post_detail.html', {"post":post})
+
+
+def post_create(request):
+  if request.method == 'POST':
+    form = PostForm(request.POST)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.user = request.user
+      post.save()
+      return redirect('post_detail', pk=post.pk)
+  else:
+    form = PostForm()
+  return render(request, 'post_form.html', {'form': form, 'header': f'New Post'})
+
+def post_edit(request, pk):
+  if request.method == "POST":
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+      post = form.save()
+      return redirect('post_detail', pk=post.pk)
+  else:
+    form = PostForm(instance=artist)
+  return render(request, 'post_form.html', {'form': form, 'header':f'Edit {post.name}'})
+
+def post_delete(request, pk):
+  Post.objects.get(id=pk).delete()
+  return redirect('post_lists')
+
+def comment_create(request, pk):
+  post = Post.objects.get(id=pk)
+  if request.method =='POST':
+    form = CommentForm(request.POST)
+    if form.is_valid():
+      comment = form.save(commit=False)
+      comment.post = post
+      comment.save()
+      return redirect('post_detail', pk=comment.post.pk)
+  else:
+    form = CommentForm()
+  return render(request, 'post_form.html', {'form': form, 'header':f'Add comment for {post.name}'})
+
+  def profile(request):
+    user = request.user
+    posts = Post.objects.filter(user=user)
+    return render(request, 'profile.html', {'posts': posts})
