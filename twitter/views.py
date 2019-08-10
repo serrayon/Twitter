885ad1db45b688561458201e2bce614c686adb68
hungfaikoo <jhk88@hotmail.com>
2019-08-09 17:28:14 -0700

    changes

--- twitter/views.py
+++ twitter/views.py
@@ -42,16 +42,16 @@ def post_create(request):
 
 def post_edit(request, pk):
     post = Post.objects.get(id=pk)
+    form = PostForm(request.POST, instance=post)
     if request.method == 'POST':
-      form = PostForm(request.POST, instance=post)
-    if form.is_valid():
-      post = form.save()
-      return redirect('post_detail', pk=post_id.pk)
+      if form.is_valid():
+        post = form.save()
+        return redirect('post_detail', pk=post.pk)
     else:
       form = PostForm(instance=post) 
     return render(request, 'post_form.html', {'form': form, 'header':f'Edit {post.content}'})
 
-def post_delete(request, pk, post_id):
+def post_delete(request, pk):
   Post.objects.get(id=pk).delete()
   return redirect('post_list')
 
