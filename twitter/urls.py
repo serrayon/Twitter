99be1edea0fbe022adeef27491068503dbcbe5c8
octavio <serrayon81@gmail.com>
2019-08-11 14:49:45 -0700

    Jacky added css style

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,10 +1,29 @@
+# from django.urls import path
+# from . import views
+# # second part to routes
+
+# urlpatterns = [
+#     # path('',views.home, name='home'),
+#     path('',views.post_list, name='post_list'),
+#     path('posts/', views.post_list, name = 'post_list'),
+#     path('posts/<int:pk>', views.post_detail,name='post_detail'),
+#     path('posts/<int:pk>/edit', views.post_edit, name='post_edit'),
+#     path('posts/<int:pk>/delete', views.post_delete, name='post_delete'),
+#     path('posts/new', views.post_create, name='post_create'),
+#     path('posts/<int:pk>/comments/new', views.comment_create, name='comment_create'),
+#     path('comments', views.comment_list, name='comment_list' ),
+#     path('status',views.json_res, name='status' ),
+#     path('profile/', views.profile, name='profile')   
+# ]
+
+
 from django.urls import path
 from . import views
-# second part to routes
+# second part to routesj
 
 urlpatterns = [
     # path('',views.home, name='home'),
-    # path('',views.welcome_page, name='welcome_page'),
+    path('',views.welcome_page, name='welcome_page'),
     path('',views.post_list, name='post_list'),
     # path('home',views.post_list, name='post_list'),
     path('posts/', views.post_list, name = 'post_list'),
--- twitter/views.py
+++ twitter/views.py
@@ -10,6 +10,9 @@ def home(request):
 def json_res(request):
     return JsonResponse({ "status" : "Ok" })
 
+def welcome_page(request):
+  return render(request, 'welcome_page.html')
+
 def post_list(request):
     posts = Post.objects.all()
     post = posts.first()
@@ -43,24 +46,22 @@ def post_create(request):
   return render(request, 'post_form.html', {'form': form, 'header': f'New Post'})
 
 def post_edit(request, pk):
-    post = Post.objects.get(id=pk)
-
-    if request.method == 'POST':
-      form = PostForm(request.POST, instance=post)
+  post = Post.objects.get(id=pk)
+  if request.method == "POST":
+    form = PostForm(request.POST, instance=post)
     if form.is_valid():
       post = form.save()
-      return redirect('post_detail', pk=post_id.pk)
-    
-    else:
-      form = PostForm(instance=post) 
-    return render(request, 'post_form.html', {'form': form, 'header':f'Edit {post.content}'})
+      return redirect('post_detail', pk=post.pk)
+  else:
+    form = PostForm(instance=post)
+  return render(request, 'post_form.html', {'form': form, 'header':f'Edit {post.content}'})
 
-def post_delete(request, pk, post_id):
+def post_delete(request, pk):
   Post.objects.get(id=pk).delete()
   return redirect('post_list')
 
 def comment_create(request, pk):
-  # post = Post.objects.get(id=pk)
+  post = Post.objects.get(id=pk)
   if request.method =='POST':
     form = CommentForm(request.POST)
     if form.is_valid():
@@ -76,3 +77,86 @@ def profile(request):
   user = request.user
   posts = Post.objects.filter(user=user)
   return render(request, 'profile.html', {'posts': posts})
+
+# from django.shortcuts import render, redirect
+# from django.http import HttpResponse, JsonResponse
+# # Create your views here.
+# from .models import Post, Comment, Profile
+# from .forms import PostForm, CommentForm, ProfileForm
+
+# def home(request):
+#     return HttpResponse("You are home")
+
+# def json_res(request):
+#     return JsonResponse({ "status" : "Ok" })
+
+# def welcome_page(request):
+#   return render(request, 'welcome_page.html')
+
+# def post_list(request):
+#     posts = Post.objects.all()
+#     post = posts.first()
+#     # comments = Comment.objects.filter(post=post.pk)
+#     # print(post.comments_post.all().first().message)
+#     # print(comments)
+#     return render(request, 'post_list.html', {"posts": posts})
+    
+
+# def comment_list(request):
+#     comments = Comment.objects.all()
+#     return render(request, 'comment_list.html', {"comments": comments})
+
+# def post_detail(request, pk):
+#     post = Post.objects.get(id=pk)
+#     comments = Comment.objects.filter(post=post)
+#     print(comments)
+#     return render(request, 'post_detail.html', {"post":post, "comments": comments})
+
+
+# def post_create(request):
+#   if request.method == 'POST':
+#     form = PostForm(request.POST)
+#     if form.is_valid():
+#       post = form.save(commit=False)
+#       post.user = request.user
+#       post.save()
+#       return redirect('post_detail', pk=post.pk)
+#   else:
+#     form = PostForm()
+#   return render(request, 'post_form.html', {'form': form, 'header': f'New Post'})
+
+# def post_edit(request, pk):
+#     post = Post.objects.get(id=pk)
+#     if request.method == 'POST':
+#       form = PostForm(request.POST, instance=post)
+#     if form.is_valid():
+#       post = form.save()
+#       return redirect('post_detail', pk=post.pk)
+#       # return redirect('post_detail', pk=post_id.pk)
+    
+#     else:
+#       form = PostForm(instance=post) 
+#     return render(request, 'post_form.html', {'form': form, 'header':f'Edit {post.content}'})
+
+# # def post_delete(request, pk, post_id):
+# def post_delete(request, pk):
+#   Post.objects.get(id=pk).delete()
+#   return redirect('post_list')
+
+# def comment_create(request, pk):
+#   post = Post.objects.get(id=pk)
+#   if request.method =='POST':
+#     form = CommentForm(request.POST)
+#     if form.is_valid():
+#       comment = form.save(commit=False)
+#       comment.post = post
+#       comment.save()
+#       return redirect('post_detail', pk=comment.post.pk)
+#   else:
+#     form = CommentForm()
+#   return render(request, 'post_form.html', {'form': form, 'header':f'Add comment for {post.user}'})
+
+# def profile(request):
+#   user = request.user
+#   posts = Post.objects.filter(user=user)
+#   return render(request, 'profile.html', {'posts': posts})
